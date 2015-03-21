e726d977645957755c0b702cf89ca08b67d1bfde
Oxana Goutnik <og@syrec.org>
2015-03-21 12:53:42 -0500

    weekstart = monday

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module TeamGlossary
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
-    config.beginning_of_week = :sunday
+    config.beginning_of_week = :monday
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
